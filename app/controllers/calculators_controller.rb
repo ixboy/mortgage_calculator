4e5c78951be2f5eb485330330f97aaa4273db677
ixboy <ismaelixboy@gmail.com>
2021-09-30 13:49:48 +0100

    add help to calculate the full amount to repay

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -8,6 +8,7 @@ class CalculatorsController < ApplicationController
       interest_rate = params[:calculate][:interest_rate].to_f
       number_of_years = params[:calculate][:number_of_years].to_f
       @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
+      @full_payment = helpers.total_loan(@monthly_payment, number_of_years) if @monthly_payment.is_a? Numeric
     else
       @calculator = Calculator.all
     end
--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -1,10 +1,16 @@
 module CalculatorsHelper
-
   def payment(property_price, down_payment, interest_rate, number_of_years)
     interest = percent_of(interest_rate)
     principal_amount = property_price - down_payment
     years = number_of_years * 12
-    (principal_amount * interest) / (1 - (1 + interest)**-years)
+    result = (principal_amount * interest) / (1 - (1 + interest)**-years)
+    result || 'please input the correct values only in numbers'
+  end
+
+  def total_loan(monthly_pay, number_of_years)
+    return 'invalid numbers' unless monthly_pay
+
+    monthly_pay * (number_of_years * 12)
   end
 
   def percent_of(n)
