a3dff6227965ffdf893e4ab382fc9f473bbf4f9e
ixboy <ismaelixboy@gmail.com>
2021-09-30 04:20:52 +0100

    add helper method to calculate the loan's monthly payment

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -1,2 +1,13 @@
 module CalculatorsHelper
+
+  def payment(property_price, down_payment, interest_rate, number_of_years)
+    interest = percent_of(interest_rate)
+    principal_amount = property_price - down_payment
+    years = number_of_years * 12
+    (principal_amount * interest) / (1 - (1 + interest)**-years)
+  end
+
+  def percent_of(n)
+    (n.to_f / 100) / 12
+  end
 end
