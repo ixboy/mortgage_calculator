6d682215debb44549025d83c9df23d43f3a4aff5
ixboy <ismaelixboy@gmail.com>
2021-09-30 04:19:12 +0100

    edit index action and delete unused methods

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,69 +1,15 @@
 class CalculatorsController < ApplicationController
-  before_action :set_calculator, only: %i[ show edit update destroy ]
+  before_action :set_calculator, only: %i[show edit update destroy]
 
-  # GET /calculators or /calculators.json
   def index
-    @calculators = Calculator.all
-  end
-
-  # GET /calculators/1 or /calculators/1.json
-  def show
-  end
-
-  # GET /calculators/new
-  def new
-    @calculator = Calculator.new
-  end
-
-  # GET /calculators/1/edit
-  def edit
-  end
-
-  # POST /calculators or /calculators.json
-  def create
-    @calculator = Calculator.new(calculator_params)
-
-    respond_to do |format|
-      if @calculator.save
-        format.html { redirect_to @calculator, notice: "Calculator was successfully created." }
-        format.json { render :show, status: :created, location: @calculator }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @calculator.errors, status: :unprocessable_entity }
-      end
+    if params[:calculate]
+      property_price = params[:calculate][:property_price].to_f
+      down_payment = params[:calculate][:down_payment].to_f
+      interest_rate = params[:calculate][:interest_rate].to_f
+      number_of_years = params[:calculate][:number_of_years].to_f
+      @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
+    else
+      @calculator = Calculator.all
     end
   end
-
-  # PATCH/PUT /calculators/1 or /calculators/1.json
-  def update
-    respond_to do |format|
-      if @calculator.update(calculator_params)
-        format.html { redirect_to @calculator, notice: "Calculator was successfully updated." }
-        format.json { render :show, status: :ok, location: @calculator }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @calculator.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /calculators/1 or /calculators/1.json
-  def destroy
-    @calculator.destroy
-    respond_to do |format|
-      format.html { redirect_to calculators_url, notice: "Calculator was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_calculator
-      @calculator = Calculator.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def calculator_params
-      params.require(:calculator).permit(:number_of_years, :property_price, :down_payment, :interest_rate)
-    end
 end
