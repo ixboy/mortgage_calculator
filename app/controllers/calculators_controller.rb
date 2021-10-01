71b34965a5ca2b1b76ba33eab5e536faccc5767a
ixboy <ismaelixboy@gmail.com>
2021-10-01 19:22:03 +0100

    fix mortgage calculations formula and change some variables

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -6,14 +6,14 @@ class CalculatorsController < ApplicationController
 
   def index
     if params[:calculate]
-      property_price = params[:calculate][:property_price].to_f
-      down_payment = params[:calculate][:down_payment].to_f
+      property_price = params[:calculate][:property_price].to_i
+      down_payment = params[:calculate][:down_payment].to_i
       interest_rate = params[:calculate][:interest_rate].to_f
-      number_of_years = params[:calculate][:number_of_years].to_f
+      number_of_years = params[:calculate][:number_of_years].to_i
       @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
       @full_payment = helpers.total_loan(@monthly_payment, number_of_years) if @monthly_payment.is_a? Numeric
     else
-      @calculator = Calculator.all
+      @calculator = 'Please performe your calculation'
     end
   end
 end
--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -1,19 +1,17 @@
 module CalculatorsHelper
   def payment(property_price, down_payment, interest_rate, number_of_years)
-    interest = percent_of(interest_rate)
+    months_in_a_year = 12
+    monthly_int = (interest_rate.to_f / 100) / months_in_a_year
     principal_amount = property_price - down_payment
-    years = number_of_years * 12
-    result = (principal_amount * interest) / (1 - (1 + interest)**-years)
-    result || 'please input the correct values only in numbers'
+    n = number_of_years * months_in_a_year
+    first = principal_amount * monthly_int * ((1 + monthly_int)**n)
+    second = ((1 + monthly_int)**n) - 1
+    first / second
   end
 
   def total_loan(monthly_pay, number_of_years)
     return 'invalid numbers' unless monthly_pay
 
     monthly_pay * (number_of_years * 12)
-  end
-
-  def percent_of(num)
-    (num.to_f / 100) / 12
   end
 end
