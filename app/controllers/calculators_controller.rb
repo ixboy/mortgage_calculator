b590892bd3afc0a87576095eee4451af72b0bead
ixboy <ismaelixboy@gmail.com>
2021-10-01 22:56:53 +0100

    simplify index action

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -5,11 +5,11 @@ class CalculatorsController < ApplicationController
   before_action :set_calculator, only: %i[show edit update destroy]
 
   def index
-    if params[:calculate]
-      property_price = params[:calculate][:property_price].to_i
-      down_payment = params[:calculate][:down_payment].to_i
-      interest_rate = params[:calculate][:interest_rate].to_f
-      number_of_years = params[:calculate][:number_of_years].to_i
+    if params
+      property_price = params[:property_price].to_i
+      down_payment = params[:down_payment].to_i
+      interest_rate = params[:interest_rate].to_f
+      number_of_years = params[:number_of_years].to_i
       @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
       @full_payment = helpers.total_loan(@monthly_payment, number_of_years) if @monthly_payment.is_a? Numeric
     else
