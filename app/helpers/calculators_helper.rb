2ba9457e1d8bcbde58f57f4a4bbbcd46d2655c30
ixboy <ismaelixboy@gmail.com>
2021-10-05 04:34:28 +0100

    change calculation formula

--- app/helpers/calculators_helper.rb
+++ app/helpers/calculators_helper.rb
@@ -1,12 +1,11 @@
 module CalculatorsHelper
   def payment(property_price, down_payment, interest_rate, number_of_years)
     months_in_a_year = 12
-    monthly_int = (interest_rate.to_f / 100) / months_in_a_year
-    principal_amount = property_price - down_payment
     n = number_of_years * months_in_a_year
-    first = principal_amount * monthly_int * ((1 + monthly_int)**n)
-    second = ((1 + monthly_int)**n) - 1
-    first / second
+    principal_amount = property_price - down_payment
+    monthly_int = (principal_amount * (interest_rate / 100)) / months_in_a_year
+    result = ((principal_amount / n) + monthly_int).ceil
+    result || 0
   end
 
   def total_loan(monthly_pay, number_of_years)
