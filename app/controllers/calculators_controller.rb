53c25dd96261bba58d2c449c13f51ea99baef753
ixboy <ismaelixboy@gmail.com>
2021-10-05 06:37:15 +0100

    refactor controller's code

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -6,20 +6,18 @@ class CalculatorsController < ApplicationController
 
   def index
     if params
-      property_price = 500000
-      down_payment = 100000 
-      interest_rate = 12 
-      number_of_years = 30 
-  
       property_price = params[:property_price].to_f
       down_payment = params[:down_payment].to_f
       interest_rate = params[:interest_rate].to_f
       number_of_years = params[:number_of_years].to_i
-      @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
-      @full_payment = helpers.total_loan(@monthly_payment, number_of_years) if @monthly_payment.is_a? Numeric
     else
-      @calculator = 'Please performe your calculation'
+      property_price = 500000
+      down_payment = 100000 
+      interest_rate = 5
+      number_of_years = 30 
     end
+    @monthly_payment = helpers.payment(property_price, down_payment, interest_rate, number_of_years)
+    @full_payment = helpers.total_loan(@monthly_payment, number_of_years) if @monthly_payment.is_a? Numeric
   end
 end
 # rubocop:enable Layout/LineLength
